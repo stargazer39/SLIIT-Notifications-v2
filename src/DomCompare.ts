a2a794189934f1fb1cb0ac048af941bbb3e20180
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-11-05 16:00:05 +0530

    Class rename

--- src/DomCompare.ts
+++ src/DomCompare.ts
@@ -1,7 +1,7 @@
 // Import dom-compare. I really dont like this. Someone make types pleasss
 const htmlCompare = require("html-compare");
 
-export function compareHTML(oldHTML : string, newHTML : string) : Promise<boolean> {
+export function compareHTML(oldHTML : string, newHTML : string) : any {
     return htmlCompare.compare(oldHTML, newHTML, {
         tagComparison: {name: 0, id: 0, attributes: 0, contents: 1}
     });
